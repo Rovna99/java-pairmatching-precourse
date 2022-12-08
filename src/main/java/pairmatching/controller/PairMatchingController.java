3909c23c8e9530dac966d0fd0fc6ab7550d5d93e
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-12-08 18:24:42 +0900

    feat: 메인 시스템을 관리할 PairMatchingContorller 생성

---
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -0,0 +1,20 @@
+package pairmatching.controller;
+
+public class PairMatchingController {
+	public void run() {
+
+	}
+
+	public void selectPairMatch() {
+
+	}
+
+	public void selectPairInquiry() {
+
+	}
+
+	public void selectPairInit() {
+
+	}
+
+}
